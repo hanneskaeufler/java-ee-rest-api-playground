467f7b8606199608ce38b255b35aa73ad83f31d0
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2016-12-18 13:28:45 +0100

    🎉 working arqullian test. I dont know why but this works.
    Also from Eclipse by "Run as -> Junit Test"

--- BooksApi-ejb/src/test/java/com/fastapps/books/BookRepositoryTest.java
+++ BooksApi-ejb/src/test/java/com/fastapps/books/BookRepositoryTest.java
@@ -2,17 +2,37 @@ package com.fastapps.books;
 
 import static org.junit.Assert.*;
 
+import javax.inject.Inject;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 
 import com.fastapps.books.BookRepository;
 
+@RunWith(Arquillian.class)
 public class BookRepositoryTest {
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "simple.war")
+				.addClass(Book.class)
+				.addClass(BookRepository.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Inject
+	BookRepository bookRepo;
 
 	@Test
 	public void test_findOne_returnsBook() {
-		BookRepository bookRepo = new BookRepository();
+		Book book = bookRepo.findOne(1);
 		
-		assertNotNull(".findOne() must return a Book instance", bookRepo.findOne(1));
+		assertNotNull(".findOne() must return a Book instance", book);
+		assertEquals("Working with Legacy Code", book.getTitle());
 	}
 
 }
