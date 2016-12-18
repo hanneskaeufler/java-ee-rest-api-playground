529ce887ee5fe4289f732935259b3f74516ef55f
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2016-12-18 12:55:24 +0100

    add property title to Book class

--- BooksApi-ejb/src/main/java/com/fastapps/books/Book.java
+++ BooksApi-ejb/src/main/java/com/fastapps/books/Book.java
@@ -4,5 +4,15 @@ import javax.persistence.Entity;
 
 @Entity
 public class Book {
+	private String title;
 
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public Book setTitle(String title) {
+		this.title = title;
+		
+		return this;
+	}
 }
